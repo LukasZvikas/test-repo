0c19c1bce4e657ebf67ba36a08d9c681f0aa6468
LukasZvikas <lzvikas1@gmail.com>
2019-06-06 10:56:18 -0400

    task: fetching posts from api successfully

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { posts: '' };
+  }
+
+  async componentDidMount() {
+    const postArr = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await postArr.json();
+    this.setState({ posts });
+  }
+
+  render() {
+    const { posts } = this.state;
+    console.log('state', posts);
+    return <div className="App" />;
+  }
 }
 
 export default App;
